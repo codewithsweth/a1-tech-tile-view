83fccc10ae3cacdef6d435f081c4b9fedbede888
Sai Sweth Koormachalam <119130522+codewithsweth@users.noreply.github.com>
2025-09-24 18:55:42 +0530

    Fix missing closing Typography tag

--- src/screens/LeaderboardTiles/sections/LeaderboardSection/components/TileView.tsx
+++ src/screens/LeaderboardTiles/sections/LeaderboardSection/components/TileView.tsx
@@ -713,6 +713,7 @@ export default function TileView() {
                             >
                               DOOR SALES
                             </Typography>
+                            </Typography>
 
                             <Box
                               sx={{
